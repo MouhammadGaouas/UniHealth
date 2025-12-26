9513f3c1c6a888d8e21b275d5ac281429e96ec4e
Your Name <you@example.com>
2025-12-26 14:58:21 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen font-sans dark:bg-black">
+      <h1 className="text-white text-4xl text-center pt-6">Home page</h1>
+    </div>
+  );
+}
